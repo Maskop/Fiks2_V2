d0bfb7c04bc9568b5b50f028d40a33c015bea5f3
Joachim Otáhal <jachym.otahal@gmail.com>
2024-11-21 22:19:05 +0100

    Just Testin...

--- Fiks2_V2/Program.cs
+++ Fiks2_V2/Program.cs
@@ -1,7 +1,23 @@
-﻿namespace Fiks2_V2 {
+﻿using Fiks2;
+
+namespace Fiks2_V2 {
     internal class Program {
         static void Main(string[] args) {
-            
+            Test();
+        }
+
+        static void Test() {
+            InputHandle input = new InputHandle("sample.in");
+            for (ushort i = 0; i < input.GetNumberOfCrews(); i++) {
+                Console.WriteLine($"Crew index: {i}");
+                var watch = System.Diagnostics.Stopwatch.StartNew();
+                watch.Start();
+                foreach (var item in input.GetAstronouts(i)) {
+                    Console.WriteLine(item.ToString());
+                }
+                watch.Stop(); Console.WriteLine($"Time elpsed in ms: {watch.ElapsedMilliseconds}");
+                Console.ReadLine();
+            }
         }
     }
 }
