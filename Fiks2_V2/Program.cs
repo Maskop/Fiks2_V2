7b3ea4f560fb68049ce08f0d6760a473cfa7b783
Joachim Otáhal <jachym.otahal@gmail.com>
2024-11-23 22:25:05 +0100

    Did testing

--- Fiks2_V2/Program.cs
+++ Fiks2_V2/Program.cs
@@ -7,11 +7,22 @@ namespace Fiks2_V2 {
             InputHandle input = new InputHandle("sample.in");
             List<ushort> trashIndices;
             for (ushort i = 0; i < input.GetNumberOfCrews(); i++) {
+                Console.WriteLine($"Crew: {i}");
+                ulong pointsToDistribute = input.GetPointsToDistribute(i);
+                Console.WriteLine($"Points to distribute: {pointsToDistribute}");
                 trashIndices = new List<ushort>();
                 Astronaut[] astronauts = input.GetAstronouts(i);
                 ushort masterIndex = Algorithm.AlgorithmPart1(astronauts, trashIndices);
-                Console.WriteLine(masterIndex + 1);
-                Console.WriteLine(String.Join(" | ", trashIndices.Select(i => i + 1)));
+                astronauts[masterIndex].WriteOutWholeTree();
+                //Algorithm.AlgorithmPart2V2(astronauts, masterIndex, input.GetPointsToDistribute(i));
+                bool failed = Algorithm.AlgorithmPart2V2(astronauts, masterIndex, trashIndices, ref pointsToDistribute);
+                Console.WriteLine("----------------------------------------------------");
+                astronauts[masterIndex].WriteOutWholeTree();
+                //Console.WriteLine(masterIndex + 1);
+                //Console.WriteLine(String.Join(" | ", trashIndices.Select(i => i + 1)));
+                Console.WriteLine($"This configuration is possible: {failed}");
+                Console.WriteLine($"Points left: {pointsToDistribute}");
+                Console.WriteLine("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
             }
         }
 
